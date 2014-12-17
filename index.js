7a5fae83ff8b2970136be0302182de9ee8dfe4cf
Brian Tonnies <brtonnies@gmail.com>
2014-12-16 22:35:55 -0600

    undoing some experiments gone bad

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Call up index.html
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
+// Console logs User Connects & Disconnects
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
@@ -16,18 +15,21 @@ io.on('connection', function(socket){
   });
 });
 
+// Console logs message & number
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
 });
 
+// Send message to entire chat room
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
+//console log init
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
